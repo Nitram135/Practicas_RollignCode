73de2245564fb5f33a574c2e6b4e20ada8f8fe16
Martin <marticicarodriguez@gmail.com>
2025-10-10 13:33:16 -0300

    primer commit de la tarea (13-10)

---
+++ codigo_Javascript/tarea(13-10).js
@@ -0,0 +1,42 @@
+function cambiarSaludo() {
+    const titulo = document.getElementById("saludo");
+    titulo.innerText = "Hola desde javascript?"
+}
+function cambiarColor(){
+    const mensaje = document.querySelector(".mensaje");
+    mensaje.style.color = "blue";
+    mensaje.style.fontSize = "24px";
+}
+
+function agregarParrafo(){
+    const nuevoParrafo = document.createElement("p");
+    nuevoParrafo.innerText = "Soy un nuevo parrafo";
+    document.getElementById("contenedor").appendChild(nuevoParrafo);
+}
+
+
+//-------------------------------------------------------------------------------------------
+// tarea de tarjeta de presentacion
+//-------------------------------------------------------------------------------------------
+
+
+const nombre = document.querySelector("#nombre");
+const email = document.querySelector("#email");
+const rol = document.querySelector("#rol");
+const image = document.querySelector("#image");
+const cabecera = document.querySelector("#cabecera");
+const puesto = document.querySelector("#puesto");
+const contacto = document.querySelector("#contacto");
+const foto = document.querySelector("#foto");
+
+function cargar(){
+   
+
+
+    cabecera.innerHTML = nombre.value;
+    contacto.innerHTML = email.value;
+    puesto.innerHTML = rol.value;
+    foto.src = image.value;
+   
+}
+
