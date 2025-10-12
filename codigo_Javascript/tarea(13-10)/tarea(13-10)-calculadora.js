40877807ca2f9003290870a28b70fe9d739b8064
Martin <marticicarodriguez@gmail.com>
2025-10-12 20:38:58 -0300

    calculadora terminada

--- codigo_Javascript/tarea(13-10)/tarea(13-10)-calculadora.js
+++ codigo_Javascript/tarea(13-10)/tarea(13-10)-calculadora.js
@@ -9,48 +9,13 @@ el boton "limpiar", se podra armar con una funcion que devuelva a "0" el value d
 
 */
 
-
-const num0 = document.querySelector(".zero");
-const num1 = document.querySelector("#num1");
-const num2 = document.querySelector("#num2");
-const num3 = document.querySelector("#num3");
-const num4 = document.querySelector("#num4");
-const num5 = document.querySelector("#num5");
-const num6 = document.querySelector("#num6");
-const num7 = document.querySelector("#num7");
-const num8 = document.querySelector("#num8");
-const num9 = document.querySelector("#num9");
-
 const visor = document.querySelector("#display");
+const botones = document.querySelectorAll("#btn");
 
-num0.addEventListener("click", ()=> {
-       visor.value = num0.innerText;
-})
-num1.addEventListener("click", ()=> {
-       visor.value = num1.innerText;
-})
-num2.addEventListener("click", ()=> {
-       visor.value = num2.innerText;
-})
-num3.addEventListener("click", ()=> {
-       visor.value = num3.innerText;
-})
-num4.addEventListener("click", ()=> {
-       visor.value = num4.innerText;
-})
-num5.addEventListener("click", ()=> {
-       visor.value = num5.innerText;
-})
-num6.addEventListener("click", ()=> {
-       visor.value = num6.innerText;
-})
-num7.addEventListener("click", ()=> {
-       visor.value = num7.innerText;
-})
-num8.addEventListener("click", ()=> {
-       visor.value = num8.innerText;
-})
-num9.addEventListener("click", ()=> {
-       visor.value = num9.innerText;
-})
-
+botones.forEach((btn)=>{
+       btn.addEventListener("click", ()=>{
+           if(btn.innerHTML === "="){visor.value = eval(visor.value);} 
+           else if (btn.innerHTML === "C"){visor.value = "";}
+           else {visor.value += btn.innerHTML}  
+       })
+});
