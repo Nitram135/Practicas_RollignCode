fa7e25327f232918603fbbc322eb0d88b13b4420
Martin <marticicarodriguez@gmail.com>
2025-10-19 11:20:29 -0300

    hemos agregado funcionalidad debusqueda a los botones del fromulario

--- codigo_Javascript/tarea(20-10)/tarea(20-10)buscador-stock.js
+++ codigo_Javascript/tarea(20-10)/tarea(20-10)buscador-stock.js
@@ -15,15 +15,31 @@ let modificar_cancelar = document.querySelector("#btn-cancelar");
 let nuevo_aceptar = document.querySelector("#btn-aceptar");
 
 class Analisis {
-    constructor(codigo, nombre,nbu,metodo,marca,indicaciones,parametros){
+    constructor(codigo, nombre,nbu,metodo,marca,indicaciones){
      this.codigo = codigo;
      this.nombre =nombre;
      this.nbu =nbu;
      this.metodo =metodo;
      this.marca =marca;
      this.indicaciones =indicaciones;
-     this.parametros =parametros;
     }
 }
+const baseDatos= [];
 
+const glucemia = new Analisis("412","GLUCEMIA","660412", "COLORIMETRICO ENZIMATICO", "GT LAB", "AYUNO DE 8 HS");
 
+codigo.addEventListener('blur', ()=>{
+      codigo.value = glucemia.codigo;
+      nombre.value = glucemia.nombre;
+      nbu.value = glucemia.nbu;
+      metodo.value = glucemia.metodo;
+      marca.value = glucemia.marca;
+      indicaciones.value = glucemia.indicaciones;
+
+      let fila = document.createElement('tr');
+      let celda = `<td>${glucemia.codigo}</td>
+                   <td>${glucemia.nombre}</td>
+                   <td>${"0"}</td>`;
+      fila.innerHTML = celda;
+      parametros.innerHTML = fila.innerHTML
+});
