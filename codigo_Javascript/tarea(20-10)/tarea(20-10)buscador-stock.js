9ef5392ad937fa4099d43ca4bcccfe0787499941
Martin <marticicarodriguez@gmail.com>
2025-10-19 21:08:16 -0300

    hemos agregado funcionalidad de guardado d edatos al formulario

--- codigo_Javascript/tarea(20-10)/tarea(20-10)buscador-stock.js
+++ codigo_Javascript/tarea(20-10)/tarea(20-10)buscador-stock.js
@@ -13,33 +13,88 @@ let importarParametros = document.querySelector("#importarParametros");
 let informeVistaPrevia = document.querySelector("#informeVistaPrevia");
 let modificar_cancelar = document.querySelector("#btn-cancelar");
 let nuevo_aceptar = document.querySelector("#btn-aceptar");
+//-------------
+//aqui crearemos variables que concideremos mas utiles
+let todosLosInputs = document.querySelectorAll("input");
 
-class Analisis {
-    constructor(codigo, nombre,nbu,metodo,marca,indicaciones){
+function Analisis (codigo,nombre,nbu, metodo,marca,indicaciones){
      this.codigo = codigo;
-     this.nombre =nombre;
-     this.nbu =nbu;
-     this.metodo =metodo;
-     this.marca =marca;
+     this.nombre = nombre;
+     this.nbu = nbu;
+     this.metodo = metodo;
+     this.marca = marca;
      this.indicaciones =indicaciones;
-    }
 }
+let botonNuevoActivado = false;
 const baseDatos= [];
 
-const glucemia = new Analisis("412","GLUCEMIA","660412", "COLORIMETRICO ENZIMATICO", "GT LAB", "AYUNO DE 8 HS");
 
-codigo.addEventListener('blur', ()=>{
-      codigo.value = glucemia.codigo;
-      nombre.value = glucemia.nombre;
-      nbu.value = glucemia.nbu;
-      metodo.value = glucemia.metodo;
-      marca.value = glucemia.marca;
-      indicaciones.value = glucemia.indicaciones;
-
-      let fila = document.createElement('tr');
-      let celda = `<td>${glucemia.codigo}</td>
-                   <td>${glucemia.nombre}</td>
-                   <td>${"0"}</td>`;
-      fila.innerHTML = celda;
-      parametros.innerHTML = fila.innerHTML
+nuevo_aceptar.addEventListener("click", ()=>{
+    if(botonNuevoActivado === false){
+        todosLosInputs.forEach((i)=>{
+          i.value = "";
+          i.removeAttribute("readonly");
+        });
+        indicaciones.removeAttribute("readonly")
+        indicaciones.value ="";
+        parametros.innerHTML = "";
+        modificar_cancelar.innerHTML = "CANCELAR";
+        modificar_cancelar.type = "reset";
+        nuevo_aceptar.innerHTML = "ACEPTAR";
+        nuevo_aceptar.type = "submit";
+        botonNuevoActivado = true;
+        focus(codigo);
+    }else{
+        if (codigo.value!=="" && nombre.value !=="" && nbu.value!==""&&metodo.value!==""&&marca.value!==""&&indicaciones.value!==""&&botonNuevoActivado===true){
+            let nuevoAnalisis = new Analisis(codigo.value,nombre.value.toUpperCase(),nbu.value,metodo.value.toUpperCase(),marca.value.toUpperCase(),indicaciones.value);
+            baseDatos.push(nuevoAnalisis)
+            todosLosInputs.forEach((i)=>{
+            i.value = "";
+            });
+            indicaciones.value ="";
+            parametros.innerHTML = "";
+            modificar_cancelar.innerHTML = "MODIFICAR";
+            modificar_cancelar.type = "button";
+            nuevo_aceptar.innerHTML = "NUEVO";
+            nuevo_aceptar.type = "button";
+            nbu.setAttribute("readonly");
+            metodo.setAttribute("readonly");
+            marca.setAttribute("readonly");
+            indicaciones.setAttribute("readonly");
+            botonNuevoActivado = false;
+            focus(codigo);
+        }else{
+            todosLosInputs.forEach((i)=>{
+            i.value = "";
+            });
+            indicaciones.value ="";
+            parametros.innerHTML = "";
+            modificar_cancelar.innerHTML = "MODIFICAR";
+            modificar_cancelar.type = "button";
+            nuevo_aceptar.innerHTML = "NUEVO";
+            nuevo_aceptar.type = "button";
+             nbu.setAttribute("readonly");
+            metodo.setAttribute("readonly");
+            marca.setAttribute("readonly");
+            indicaciones.setAttribute("readonly");
+            botonNuevoActivado = false;
+            focus(codigo);
+            alert(`Faltan ingreasar Datos`);
+        }
+    } 
 });
+
+
+
+
+
+
+
+// codigo.addEventListener('blur', ()=>{
+//       let fila = document.createElement('tr');
+//       let celda = `<td>${glucemia.codigo}</td>
+//                    <td>${glucemia.nombre}</td>
+//                    <td>${"0"}</td>`;
+//       fila.innerHTML = celda;
+//       parametros.innerHTML = fila.innerHTML
+// });
